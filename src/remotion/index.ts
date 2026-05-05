07c3bf1d3b4b57a2f0462d013cdda7e31224d042
waseemsanaa772 <waseemsanaa772@gmail.com>
2026-05-05 21:38:29 +0300

    Fix all syntax errors and use correct hooks

--- src/remotion/index.ts
+++ src/remotion/index.ts
@@ -1,4 +1,4 @@
-import { registerRoot, Composition, AbsoluteFill, Sequence, useCurrentFrame, useVideoConfig, Audio, staticFile, Img } from 'remotion';
+import { registerRoot, Composition, AbsoluteFill, Sequence, useCurrentFrame, useVideoConfig, Audio, Img, staticFile } from 'remotion';
 import { interpolate } from 'remotion';
 
 const Scene1: React.FC = () => {
@@ -9,7 +9,7 @@ const Scene1: React.FC = () => {
   const imageFile = `${imageIndex + 1}.png`;
   const scale = interpolate(frame % durationPerImage, [0, durationPerImage], [1, 1.05]);
   const opacity = interpolate(frame % durationPerImage, [0, 15], [0, 1], { extrapolateLeft: 'clamp', extrapolateRight: 'clamp' });
-  
+
   return (
     <AbsoluteFill style={{ backgroundColor: 'black' }}>
       <Img src={staticFile(`images/${imageFile}`)} style={{
@@ -41,7 +41,7 @@ const Scene2: React.FC = () => {
   const imageFile = `${imageIndex + 5}.png`;
   const scale = interpolate(frame % durationPerImage, [0, durationPerImage], [1, 1.08]);
   const brightness = interpolate(frame % durationPerImage, [0, durationPerImage], [1, 1.15]);
-  
+
   return (
     <AbsoluteFill>
       <Img src={staticFile(`images/${imageFile}`)} style={{
@@ -70,7 +70,7 @@ const AlMutanabbiFilm: React.FC = () => {
   const { fps } = useVideoConfig();
   const scene1Duration = fps * 3 * 4;
   const scene2Duration = fps * 4 * 6;
-  
+
   return (
     <>
       <Sequence from={0} durationInFrames={scene1Duration}>
@@ -87,7 +87,7 @@ registerRoot(() => (
   <Composition
     id="MyComp"
     component={AlMutanabbiFilm}
-    durationInFrames={ (3*4 + 4*6) * 30 }
+    durationInFrames={(3 * 4 + 4 * 6) * 30}
     fps={30}
     width={1080}
     height={1920}
