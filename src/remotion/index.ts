7f95208349a8a175b82c144817997c8e5dbe3648
waseemsanaa772 <waseemsanaa772@gmail.com>
2026-05-05 22:04:19 +0300

    Fix registerRoot syntax and string interpolation

--- src/remotion/index.ts
+++ src/remotion/index.ts
@@ -6,7 +6,7 @@ const Scene1 = () => {
   const durationPerImage = fps * 2;
   const imageIndex = Math.min(Math.floor(frame / durationPerImage), 3);
   const imageFile = `${imageIndex + 1}.png`;
-  
+
   return (
     <AbsoluteFill style={{ backgroundColor: 'black' }}>
       <Img src={staticFile(`images/${imageFile}`)} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
@@ -21,9 +21,9 @@ const Scene2 = () => {
   const durationPerImage = fps * 2;
   const imageIndex = Math.min(Math.floor(frame / durationPerImage), 5);
   const imageFile = `${imageIndex + 5}.png`;
-  
+
   return (
-    <AbsoluteFill>
+    <AbsoluteFill style={{ backgroundColor: 'black' }}>
       <Img src={staticFile(`images/${imageFile}`)} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
       <Audio src={staticFile('audio/scene2.mp3')} />
     </AbsoluteFill>
@@ -33,14 +33,12 @@ const Scene2 = () => {
 const FullFilm = () => {
   const { fps } = useVideoConfig();
   return (
-    <>
-      <Sequence from={0} durationInFrames={fps * 8}>
-        <Scene1 />
-      </Sequence>
+    <Sequence from={0} durationInFrames={fps * 8}>
+      <Scene1 />
       <Sequence from={fps * 8} durationInFrames={fps * 12}>
         <Scene2 />
       </Sequence>
-    </>
+    </Sequence>
   );
 };
 
